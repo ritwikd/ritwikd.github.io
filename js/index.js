f05f648fddd6590b8059e9135ba361a816701145
Ritwik Dutta <ritzymail@gmail.com>
2014-08-23 18:46:41 -0700

    Adding everything.

---
+++ js/index.js
@@ -0,0 +1,110 @@
+var postTemplate = [
+	'<div class="post wrapper">' + 
+	'<div class="post title centered bold">',
+	'</div><div class="post date centered">',
+	'</div><div class="post body">',
+	'</div></div>'
+];
+
+var staticContent = {
+	'About' : 'Hello. I\'m a student, programmer, and web-developer, located in California. I am currently working on finishing high school and expanding my technological skillset. <br><br> ' +
+	'In my personal time, I enjoy making websites, listening to music, playing video games, watching movies and TV, and reading books. <br><br>' + 
+	'If you want to contact me, shoot me an email, and I\'ll respond as soon as possible.' +
+	'<h3>Skills</h3><ul><li><h5>Python</h5></li><li><h5>Java</h5></li><li><h5>Linux</h5></li><li><h5>SDL</h5></li><li><h5>Unreal</h5></li><li><h5>HTML</h5></li><li><h5>CSS</h5></li><li><h5>JS</h5></li></ul>',
+	'Projects' : 'Here are some of my recent projects. <br>' +
+				'<h3 class="project title">GT Health Dashboard</h3>' +
+					'<div class="project description">The GT Health Dashboard is a software project I implemented at Georgia Tech over the summer under the guidance of a professor. It is about creating a free and open-source end-to-end software system for monitoring long term care patients in smart homes (report here).' +
+					'<br><div class="project links"><a class="project link" href="http://gtd.ritwikd.com">home</a> <a class="project link" href="http://github.com/ritwikd/gt-dashboard/">github</a></div>' +
+				'<h3 class="project title">Max Keyboards Key Configuration Utility</h3>' +
+					'<div class="project description">I created this keyboard configuration utility for Max Keyboards. It can be used to create a customized keyboard with a variety of layouts and options. Each key can be individually customized with respect to LED and keycap color. Every key on the page is rendered entirely in CSS. I created this entire page from scratch.' +
+					'<br><div class="project links"><a class="project link" href="http://maxkey.ritwikd.com">home</a>' +
+				'<h3 class="project title">K.A.R.E</h3>' +
+					'<div class="project description">K.A.R.E is a project that I started at a hackathon with some friends. It is a GitHub recommendation engine that provides repositories similar to user selected repositories. I worked on some of the frontend and backend components of this project.' +
+					'<br><div class="project links"><a class="project link" href="http://kare.progger.io">home</a> <a class="project link" href="https://github.com/arshsab/K.A.R.E">github</a></div>' +
+				'<h3 class="project title">securewallet</h3>' +
+					'<div class="project description">securewallet is a cool little password manager I wrote as a side project. It uses 128-bit AES encryption. It\'s written in pure Python, so theoretically it should work on any platform, but I\'ve only tested on Linux. I wrote this entire application.' +
+					'<br><div class="project links"><a class="project link" href="http://securewallet.ritwikd.com">home</a> <a class="project link" href="http://github.com/ritwikd/securewallet/">github</a></div>',
+	'Contact' : 'Contact',
+}
+
+var currentLink = '';
+
+function generatePost(postTitle, postDate, postBody) {
+	var postHTML = '';
+	postHTML += postTemplate[0] + postTitle;
+	postHTML += postTemplate[1] + postDate.substring(0, 16);
+	postHTML += postTemplate[2] + postBody;
+	postHTML += postTemplate[3];
+	return postHTML;
+}
+
+function addPost(postTitle, postDate, postBody, postHolderElem) {
+	var postHTML = generatePost(postTitle, postDate, postBody);
+	console.log(postHTML)
+	postHolderElem.append(postHTML);
+}
+
+function getBlogData() {
+	var blogPost = {};
+	blogData = $.getJSON('http://ritwikdutta.tumblr.com/api/read/json?callback=?',
+		function(response) {
+        	$.each(response.posts, function(postIndex) {
+        		blogPost = response.posts[postIndex];
+        		addPost(blogPost['regular-title'],
+        			blogPost.date,
+        			blogPost['regular-body'],
+        			$('.content.wrapper'));
+        	})
+
+    });
+}
+
+function fadeOut() {
+	$(".content.wrapper").animate( {
+		opacity : 0
+	}, 150);
+}
+
+function fadeIn() {
+	$(".content.wrapper").animate( {
+		opacity : 1
+	}, 150);
+}
+
+function loadLink(link) {
+	var linkType = $(link).attr('data-type');
+	loadData(linkType);
+}
+
+function loadData(linkType) {
+	fadeOut();
+	$('.content.wrapper').html('');	
+	console.log($('*[data-type="' + linkType + '"]')[0])
+	if (linkType === '') {
+		location.hash = '#About';
+		loadData(location.hash.substring(1));
+	} else {
+		$('.header.nav.link.bold')[0].className = $('.header.nav.link.bold')[0].className.substring(0, 16);
+		$('*[data-type="' + linkType + '"]')[0].className += ' bold';
+		switch(linkType) {
+			case 'About' :
+				$(".content.wrapper").append(staticContent[linkType]);
+				break;
+			case 'Projects' :
+				$(".content.wrapper").append(staticContent[linkType]);
+				break;
+			case 'Resume':
+				window.location = 'assets/resume.pdf';
+				break;
+			case 'Blog':
+				getBlogData();
+				break;
+			case 'Contact':
+				$(".content.wrapper").append(staticContent[linkType]);
+				break;
+		}
+	}
+	fadeIn();
+}
+
+loadData(location.hash.substring(1));
