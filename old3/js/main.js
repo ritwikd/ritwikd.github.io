445e17d121026cee493cba4bd57ef61f0919d82b
Ritwik Dutta <ritzymail@gmail.com>
2022-08-21 00:02:35 -0700

    New site.
    Update.

--- old3/js/main.js
+++ old3/js/main.js
@@ -2,27 +2,27 @@ $(document).ready(function() {
 
     /* ======= Scrollspy ======= */
    $('body').scrollspy({ target: '#page-nav-wrapper', offset: 100});
-    
+
     /* ======= ScrollTo ======= */
     $('.scrollto').on('click', function(e){
-        
+
         //store hash
         var target = this.hash;
-                
+
         e.preventDefault();
-        
+
 		$('body').scrollTo(target, 800, {offset: -60, 'axis':'y'});
 		
 	});
-	
-	/* ======= Fixed page nav when scrolled ======= */    
+
+	/* ======= Fixed page nav when scrolled ======= */
     $(window).on('scroll resize load', function() {
-        
+
         $('#page-nav-wrapper').removeClass('fixed');
-         
+
          var scrollTop = $(this).scrollTop();
          var topDistance = $('#page-nav-wrapper').offset().top;
-         
+
          if ( (topDistance) > scrollTop ) {
             $('#page-nav-wrapper').removeClass('fixed');
             $('body').removeClass('sticky-page-nav');
@@ -33,26 +33,26 @@ $(document).ready(function() {
          }
 
     });
-	
 
-    
+
+
     /* ======= Isotope plugin ======= */
     /* Ref: http://isotope.metafizzy.co/ */
-    // init Isotope    
+    // init Isotope
     var $container = $('.isotope');
-    
+
     $container.imagesLoaded(function () {
         $('.isotope').isotope({
             itemSelector: '.item'
         });
     });
-    
+
     // filter items on click
     $('#filters').on( 'click', '.type', function() {
       var filterValue = $(this).attr('data-filter');
       $container.isotope({ filter: filterValue });
     });
-    
+
     // change is-checked class on buttons
     $('.filters').each( function( i, typeGroup ) {
         var $typeGroup = $( typeGroup );
@@ -61,6 +61,6 @@ $(document).ready(function() {
           $( this ).addClass('active');
         });
     });
-    
+
 
 });
